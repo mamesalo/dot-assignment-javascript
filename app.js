944415bfe0ec6a0566d793ec907dd055891dd60e
mohammed saleh <mamesalo7337@gmail.com>
2024-09-30 09:57:48 +0300

    javascript two

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-function calculateTriangleArea() {
-  const s = (5 + 6 + 7) / 2;
-  const area = Math.sqrt(s * (s - 5) * (s - 6) * (s - 7));
-  const result = document.getElementById("triangle");
-  result.innerHTML = area;
+function turnOnLight() {
+  document.getElementById("bulb").src = "bulb-on.png";
 }
-function calculateMultiplication() {
-  const num1 = document.getElementById("num1");
-  const num2 = document.getElementById("num2");
-  const multiply = num1.value * num2.value;
-  const divide = num1.value / num2.value;
-  const multiplyResult = document.getElementById("multiplication");
-  const divideResult = document.getElementById("division");
-  multiplyResult.innerHTML = num1.value + " * " + num2.value + " = " + multiply;
-  divideResult.innerHTML = num1.value + " / " + num2.value + " = " + divide;
+function turnOffLight() {
+  document.getElementById("bulb").src = "bulb-off.png";
 }
-function createString() {
-  const string = document.getElementById("text1");
-  const newString = "JavaScript " + string.value;
-  document.getElementById("string").innerHTML = newString;
+function getTime() {
+  const currentTime = new Date().toLocaleTimeString("en-US", {
+    hour: "numeric",
+    hour12: true,
+    minute: "numeric",
+    second: "numeric",
+  });
+  document.getElementById("time").innerHTML = "Time: " + currentTime;
+}
+function modifiyString() {
+  const value = document.getElementById("text1").value;
+  const str = value.substr(1, value.length - 2);
+
+  document.getElementById("string").innerHTML = str;
 }
