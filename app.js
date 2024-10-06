0785e718a199cf0e33f16af07d9c5bfa9a5b2627
mohammed saleh <mamesalo7337@gmail.com>
2024-10-06 04:07:48 +0300

    javascript-5

--- app.js
+++ app.js
@@ -1,102 +1,89 @@
-let itemsArray = [];
-function calculateSum() {
-  const num1 = document.getElementById("t1-num1");
-  const num2 = document.getElementById("t1-num2");
-
-  if (num1.value.length == 0 || num2.value.length == 0) {
-    document.getElementById("t1-error").innerHTML =
-      "pls enter all input fields";
+function checkLeapYear() {
+  const year = parseInt(document.getElementById("leap-year").value);
+  if (!year) {
+    document.getElementById("leap-year-result").innerHTML = "Enter a year";
     return;
   }
-  document.getElementById("t1-error").innerHTML = "";
-  let result = parseInt(num1.value) + parseInt(num2.value);
-  if (num1.value == num2.value) {
-    result *= 3;
-    document.getElementById("t1-triple").innerHTML =
-      "The two entered number are equal the triple of the sum will be shown";
-    document.getElementById("t1-result").innerHTML = "Sum = " + result;
+  if (year <= 0) {
+    document.getElementById("leap-year-result").innerHTML = "Invalid year";
+    return;
+  }
+  document.getElementById("leap-year-result").innerHTML = "";
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        document.getElementById("leap-year-result").innerHTML =
+          year + " is a leap year. ✅";
+      } else {
+        document.getElementById("leap-year-result").innerHTML =
+          year + " is not a leap year. ❌";
+      }
+    } else {
+      document.getElementById("leap-year-result").innerHTML =
+        year + " is a leap year. ✅";
+    }
   } else {
-    document.getElementById("t1-triple").innerHTML = "";
-    document.getElementById("t1-result").innerHTML = "Sum = " + result;
+    document.getElementById("leap-year-result").innerHTML =
+      year + " is not a leap year. ❌";
   }
 }
-function guess() {
-  const randomNum = Math.floor(Math.random() * 10) + 1;
-  const num = document.getElementById("t2-num");
-  if (num.value.length == 0) {
-    document.getElementById("t2-error").innerHTML = "pls enter a number";
+
+function findSunday() {
+  const tableBody = document.getElementById("table-body");
+  tableBody.innerHTML = "";
+  for (let year = 2014; year <= 2050; year++) {
+    const tableRow = document.createElement("tr");
+    const columnYear = document.createElement("td");
+    const columnSunday = document.createElement("td");
+    columnYear.textContent = year;
+    let date = new Date(year, 0, 1); // 0 is for January and 1 is for the first day
+    if (date.getDay() === 0) {
+      // 0 is represent sunday in getDay
+      columnSunday.textContent = `✅`;
+    } else {
+      columnSunday.textContent = `❌`;
+    }
+    tableRow.appendChild(columnYear);
+    tableRow.appendChild(columnSunday);
+    tableBody.appendChild(tableRow);
+  }
+}
+
+function iterates() {
+  const tableBody = document.getElementById("table-body-iterates");
+  tableBody.innerHTML = "";
+
+  for (let i = 0; i <= 15; i++) {
+    const tableRow = document.createElement("tr");
+    const columnNumber = document.createElement("td");
+    const columnOdd = document.createElement("td");
+    const columnEven = document.createElement("td");
+    columnNumber.textContent = i;
+    if (i % 2 == 0) {
+      columnEven.textContent = `✅`;
+    } else {
+      columnOdd.textContent = `✅`;
+    }
+    tableRow.appendChild(columnNumber);
+    tableRow.appendChild(columnOdd);
+    tableRow.appendChild(columnEven);
+    tableBody.appendChild(tableRow);
+  }
+}
+function palindrome() {
+  const value = document.getElementById("text-palindrome").value;
+  if (value.length <= 0) {
+    console.log("empty");
+    document.getElementById("palindrome-result").innerHTML =
+      "Pls Enter String ";
     return;
   }
-  document.getElementById("t2-error").innerHTML = "";
-  if (num.value == randomNum) {
-    document.getElementById("t2-result").innerHTML = "Good Work👌";
+  const reverseStr = value.split("").reverse().join("");
+  if (value.toLowerCase() === reverseStr.toLowerCase()) {
+    document.getElementById("palindrome-result").innerHTML =
+      value + " is palindrome ✅";
   } else {
-    document.getElementById("t2-result").innerHTML =
-      " Not matched try again 😣";
+    document.getElementById("palindrome-result").innerHTML =
+      value + " is not palindrome ❌";
   }
-  document.getElementById("t2-random-num").innerHTML =
-    "The Random number: " + randomNum;
-}
-function checkRange() {
-  const num1 = document.getElementById("t3-num1");
-  const num2 = document.getElementById("t3-num2");
-  const range = 50.99;
-  if (num1.value.length == 0 || num2.value.length == 0) {
-    document.getElementById("t3-error").innerHTML =
-      "pls enter all input fields";
-    return;
-  }
-  document.getElementById("t3-error").innerHTML = "";
-  if (parseFloat(num1.value) <= range && parseFloat(num2.value) <= range) {
-    document.getElementById("t3-result").innerHTML = "result = " + true + "✅";
-  } else {
-    document.getElementById("t3-result").innerHTML = "result = " + false + "❌";
-  }
-}
-function findClosest() {
-  const num1 = document.getElementById("t4-num1");
-  const num2 = document.getElementById("t4-num2");
-
-  if (num1.value.length == 0 || num2.value.length == 0) {
-    document.getElementById("t4-error").innerHTML =
-      "pls enter all input fields";
-    return;
-  }
-
-  const x = Math.abs(100 - parseFloat(num1.value));
-  const y = Math.abs(100 - parseFloat(num2.value));
-  if (x == y) {
-    document.getElementById("t4-result").innerHTML = "both number are equal";
-  } else if (x < y) {
-    document.getElementById("t4-result").innerHTML =
-      "Number " + num1.value + " is closest value to 100";
-  } else {
-    document.getElementById("t4-result").innerHTML =
-      "Number " + num2.value + " is closest value to 100";
-  }
-  const value = document.getElementById("text1").value;
-  const str = value.substr(1, value.length - 2);
-
-  document.getElementById("string").innerHTML = str;
-}
-function add() {
-  const num = document.getElementById("t5-num");
-  if (num.value.length == 0) {
-    document.getElementById("t5-error").innerHTML = "pls enter a number";
-    return;
-  }
-  document.getElementById("t5-error").innerHTML = "";
-  itemsArray.push(num.value);
-  num.value = "";
-}
-function display() {
-  const itemList = document.getElementById("item-list");
-  itemList.innerHTML = "";
-
-  // Loop through the array and create list items
-  itemsArray.forEach(function (item, index) {
-    const listItem = document.createElement("li");
-    listItem.textContent = `Element ${index + 1} =  ${item}`;
-    itemList.appendChild(listItem);
-  });
 }
